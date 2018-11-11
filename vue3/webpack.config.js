f8887f28a0a5c9762dc8361a25259d5d103abd8c
famensaodiseng <yangfangtao110@vip.qq.com>
2018-11-11 23:13:22 +0800

    gulp

--- vue3/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-module.exports = {
-    mode :'development',
-    entry:"./src/sound.js",
-    output:{
-        path:path.join(__dirname,'./dist'),
-        filename:"bundle.js"
-    }
-}
