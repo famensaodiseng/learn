b20f65d9045ffd6ac73e8a3d27ba416847f6924b
famensaodiseng <yangfangtao110@vip.qq.com>
2019-03-25 21:33:24 +0800

    vue1

--- node4/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-aoo.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname })
-})
-
-module.exports = router
